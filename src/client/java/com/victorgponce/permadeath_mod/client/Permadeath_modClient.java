ab790b4bbfa030972c306c286252612e467aa75e
ponchisao326_ <ponchisao326@hotmail.com>
2025-04-10 20:01:04 +0200

    Initial commit

---
+++ src/client/java/com/victorgponce/permadeath_mod/client/Permadeath_modClient.java
@@ -0,0 +1,10 @@
+package com.victorgponce.permadeath_mod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Permadeath_modClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/victorgponce/permadeath_mod/Permadeath_mod.java
@@ -0,0 +1,10 @@
+package com.victorgponce.permadeath_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Permadeath_mod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
