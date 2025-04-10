f771dff13df029355704d60d11169aac53657665
ponchisao326_ <ponchisao326@hotmail.com>
2025-04-11 01:32:31 +0200

    Added a method to listen when a player join, then it insert the player into the database or update the last connection field

---
+++ src/main/java/com/victorgponce/permadeath_mod/network/PlayerJoinListener.java
@@ -0,0 +1,44 @@
+package com.victorgponce.permadeath_mod.network;
+
+import com.victorgponce.permadeath_mod.data.DataBaseHandler;
+import com.victorgponce.permadeath_mod.util.ConfigFileManager;
+import net.fabricmc.fabric.api.networking.v1.PacketSender;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlayerJoinListener implements ServerPlayConnectionEvents.Join {
+    @Override
+    public void onPlayReady(ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) {
+        ServerPlayerEntity player = handler.getPlayer();
+
+        String playerName = player.getName().toString();
+
+        // Connection to the DB
+        HashMap<Integer, String> lines;
+        lines = ConfigFileManager.readFile();
+
+        String url = lines.get(1);
+        String user = lines.get(2);
+        String password = lines.get(3);
+
+        // Regex for URL validating
+        Pattern pattern = Pattern.compile("^jdbc:mysql://([\\w.-]+)(?::(\\d+))?/([\\w]+)$");
+        Matcher matcher = pattern.matcher(url);
+
+        if (!matcher.matches()) {
+            throw new RuntimeException("Invalid URL found (line 1) on the config file, must be in this format: jdbc:mysql://BDIP:3306/your_database");
+        }
+
+        String sql = "INSERT INTO Players (Username, LastConnection) " +
+                "VALUES ('" + playerName + "', CURRENT_TIMESTAMP) " +
+                "ON DUPLICATE KEY UPDATE LastConnection = CURRENT_TIMESTAMP";;
+
+        DataBaseHandler.databaseConnector(url, user, password, sql);
+    }
+}
