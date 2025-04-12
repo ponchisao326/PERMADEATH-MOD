fbfb41e0c8432a3408a56894493e3bb9506cd5c6
ponchisao326_ <ponchisao326@hotmail.com>
2025-04-13 01:51:58 +0200

    Added the method to restrict night skip when required (non working, have to check it)

--- src/main/java/com/victorgponce/permadeath_mod/mixin/common/OnSleepListener.java
+++ src/main/java/com/victorgponce/permadeath_mod/mixin/common/OnSleepListener.java
@@ -1,8 +1,36 @@
 package com.victorgponce.permadeath_mod.mixin.common;
 
+import com.victorgponce.permadeath_mod.util.ConfigFileManager;
 import net.minecraft.server.world.SleepManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashMap;
 
 @Mixin(SleepManager.class)
 public class OnSleepListener {
+
+    @Inject(method = "getNightSkippingRequirement", at = @At("HEAD"), cancellable = true)
+    private void nightSkippingReq(int percentage, CallbackInfoReturnable<Integer> cir) {
+        // File Reader
+        HashMap<Integer, String> lines = ConfigFileManager.readFile();
+
+        if (lines.get(4).equals("10")) {
+            cir.setReturnValue(4);
+        }
+
+    }
+
+    @Inject(method = "canSkipNight", at = @At("HEAD"), cancellable = true)
+    private void skipNightprohibited(int percentage, CallbackInfoReturnable<Boolean> cir) {
+        // File reader
+        HashMap<Integer, String> lines = ConfigFileManager.readFile();
+
+        if (lines.get(4).equals("20")) {
+            cir.setReturnValue(false);
+        }
+    }
+
 }
