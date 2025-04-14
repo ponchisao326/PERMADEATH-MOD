24fcbe3cca6890df2dc7f7122235425575ad88ad
ponchisao326_ <ponchisao326@hotmail.com>
2025-04-14 02:20:45 +0200

    Created the class to set the custom skeleton as passenger on the spider

---
+++ src/main/java/com/victorgponce/permadeath_mod/mixin/day20/CustomSkeletonsMixin.java
@@ -0,0 +1,121 @@
+package com.victorgponce.permadeath_mod.mixin.day20;
+
+import com.victorgponce.permadeath_mod.util.ConfigFileManager;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.SkeletonEntity;
+import net.minecraft.entity.mob.SpiderEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.HashMap;
+
+@Mixin(ServerWorld.class)
+public class CustomSkeletonsMixin {
+
+    @Inject(method = "addEntity", at = @At("HEAD"))
+    private void onEntitySpawn(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        HashMap<Integer, String> lines = ConfigFileManager.readFile();
+
+        int day = Integer.parseInt(lines.get(4));
+
+        if (entity instanceof SpiderEntity && day >= 20) {
+            World world = entity.getWorld();
+
+            // We only act on the server
+            if (world instanceof ServerWorld serverWorld) {
+                // Create a skeleton instance
+                int skeletonType = Random.create().nextInt(5);
+                SkeletonEntity skeleton = createCustomSkeleton(serverWorld, skeletonType);
+
+                // Position the skeleton where the spider is (you can adjust an offset if desired)
+                skeleton.refreshPositionAndAngles(entity.getX(), entity.getY(), entity.getZ(), entity.getYaw(), entity.getPitch());
+                // Add the skeleton as a passenger to the spider.
+                // Ensure the passenger entity is registered in the world.
+                serverWorld.spawnEntity(skeleton);
+                skeleton.startRiding(entity);
+            }
+        }
+    }
+
+    @Unique
+    private SkeletonEntity createCustomSkeleton(ServerWorld world, int type) {
+        SkeletonEntity skeleton = new SkeletonEntity(EntityType.SKELETON, world);
+
+        switch (type) {
+            case 0:
+                // Type 0: Nothing Special
+                ItemStack normalBow = new ItemStack(Items.BOW);
+                skeleton.equipStack(EquipmentSlot.HEAD, new ItemStack(Items.DIAMOND_HELMET));
+                skeleton.equipStack(EquipmentSlot.CHEST, new ItemStack(Items.DIAMOND_CHESTPLATE));
+                skeleton.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.DIAMOND_LEGGINGS));
+                skeleton.equipStack(EquipmentSlot.FEET, new ItemStack(Items.DIAMOND_BOOTS));
+                skeleton.equipStack(EquipmentSlot.MAINHAND, normalBow);
+                skeleton.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(20.0);
+                skeleton.setHealth(20);
+                break;
+            case 1:
+                // Type 1: Punch XX
+                ItemStack bow = new ItemStack(Items.BOW);
+                bow.addEnchantment(world.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.PUNCH), 20);
+                skeleton.equipStack(EquipmentSlot.HEAD, new ItemStack(Items.CHAINMAIL_HELMET));
+                skeleton.equipStack(EquipmentSlot.CHEST, new ItemStack(Items.CHAINMAIL_CHESTPLATE));
+                skeleton.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.CHAINMAIL_LEGGINGS));
+                skeleton.equipStack(EquipmentSlot.FEET, new ItemStack(Items.CHAINMAIL_BOOTS));
+                skeleton.equipStack(EquipmentSlot.MAINHAND, bow);
+                skeleton.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(40.0);
+                skeleton.setHealth(40);
+                break;
+            case 2:
+                // Type 2: Iron Axe
+                ItemStack ironAxe = new ItemStack(Items.IRON_AXE);
+                ironAxe.addEnchantment(world.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.FIRE_ASPECT), 2);
+                skeleton.equipStack(EquipmentSlot.HEAD, new ItemStack(Items.IRON_HELMET));
+                skeleton.equipStack(EquipmentSlot.CHEST, new ItemStack(Items.IRON_CHESTPLATE));
+                skeleton.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.IRON_LEGGINGS));
+                skeleton.equipStack(EquipmentSlot.FEET, new ItemStack(Items.IRON_BOOTS));
+                skeleton.equipStack(EquipmentSlot.MAINHAND, ironAxe);
+                skeleton.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(20.0);
+                skeleton.setHealth(20);
+                break;
+            case 3:
+                // Type 3: Sharp 20
+                ItemStack crossbow = new ItemStack(Items.CROSSBOW);
+                crossbow.addEnchantment(world.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.SHARPNESS), 20);
+                skeleton.equipStack(EquipmentSlot.HEAD, new ItemStack(Items.GOLDEN_HELMET));
+                skeleton.equipStack(EquipmentSlot.CHEST, new ItemStack(Items.GOLDEN_CHESTPLATE));
+                skeleton.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.GOLDEN_LEGGINGS));
+                skeleton.equipStack(EquipmentSlot.FEET, new ItemStack(Items.GOLDEN_BOOTS));
+                skeleton.equipStack(EquipmentSlot.MAINHAND, crossbow);
+                skeleton.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(40.0);
+                skeleton.setHealth(40);
+                break;
+            case 4:
+                // Type 4: Power X
+                ItemStack bowX = new ItemStack(Items.BOW);
+                bowX.addEnchantment(world.getRegistryManager().getOrThrow(RegistryKeys.ENCHANTMENT).getOrThrow(Enchantments.POWER), 10);
+                skeleton.equipStack(EquipmentSlot.HEAD, new ItemStack(Items.LEATHER_HELMET));
+                skeleton.equipStack(EquipmentSlot.CHEST, new ItemStack(Items.LEATHER_CHESTPLATE));
+                skeleton.equipStack(EquipmentSlot.LEGS, new ItemStack(Items.LEATHER_LEGGINGS));
+                skeleton.equipStack(EquipmentSlot.FEET, new ItemStack(Items.LEATHER_BOOTS));
+                skeleton.equipStack(EquipmentSlot.MAINHAND, bowX);
+                skeleton.getAttributeInstance(EntityAttributes.MAX_HEALTH).setBaseValue(40.0);
+                skeleton.setHealth(40);
+                break;
+        }
+
+        return skeleton;
+    }
+}
