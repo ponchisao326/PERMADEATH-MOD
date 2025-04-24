5404a7c9f4232916434b7b0ef95e6d388c7825ec
Ponchisao3262 <ponchisao326@digitechfp.com>
2025-04-24 14:02:29 +0200

    Added the totem fail probability (1% failure)

---
+++ src/main/java/com/victorgponce/permadeath_mod/mixin/day30/onTotemActivation.java
@@ -0,0 +1,25 @@
+package com.victorgponce.permadeath_mod.mixin.day30;
+
+import com.victorgponce.permadeath_mod.util.ConfigFileManager;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class onTotemActivation {
+
+    @Inject(method = "tryUseDeathProtector", at = @At("HEAD"), cancellable = true)
+    public void onTotemActivationFunction(DamageSource source, CallbackInfoReturnable<Boolean> cir) {
+        int day = ConfigFileManager.readConfig().getDay();
+        if (day < 30) return;
+        // Grants the 1%
+        if (Random.create().nextInt(100) >= 1) return;
+
+        cir.setReturnValue(false);
+    }
+
+}
