0db062759c2c161d99e48e8ca39ced3e241fdddb
ponchisao326_ <ponchisao326@hotmail.com>
2025-04-28 01:18:59 +0200

    Augmented the weight of the ghasts

--- src/main/java/com/victorgponce/permadeath_mod/mobs/EndSpawnConfig.java
+++ src/main/java/com/victorgponce/permadeath_mod/mobs/EndSpawnConfig.java
@@ -6,19 +6,23 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 
+import static com.victorgponce.permadeath_mod.Permadeath_mod.LOGGER;
+
 public class EndSpawnConfig {
     public static void init() {
         int day = ConfigFileManager.readConfig().getDay();
         if (day < 30) return;
+        LOGGER.info("[PermadeathMod] Configurando spawns del End (Día: " + day + ")");
         // Selects all biomes in the End (Highlands, Midlands, Small Islands…)
         BiomeModifications.addSpawn(
                 BiomeSelectors.foundInTheEnd(),
                 SpawnGroup.MONSTER,
                 EntityType.GHAST,
-                8,  // spawn weight
-                1,   // minimum group size
-                4    // maximum group size
+                65,  // peso de spawn mayor
+                1,   // mínimo 1
+                3    // máximo 1
         );
+
         BiomeModifications.addSpawn(
                 BiomeSelectors.foundInTheEnd(),
                 SpawnGroup.MONSTER,
