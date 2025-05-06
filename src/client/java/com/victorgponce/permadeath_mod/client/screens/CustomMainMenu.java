8f27cceeab24ddd64901c2e69badd3c9a0d88c09
Ponchisao3262 <ponchisao326@digitechfp.com>
2025-05-06 14:25:32 +0200

    Buttons and Idioms

--- src/client/java/com/victorgponce/permadeath_mod/client/screens/CustomMainMenu.java
+++ src/client/java/com/victorgponce/permadeath_mod/client/screens/CustomMainMenu.java
@@ -2,11 +2,22 @@ package com.victorgponce.permadeath_mod.client.screens;
 
 import com.victorgponce.permadeath_mod.client.config.ClientConfig;
 import com.victorgponce.permadeath_mod.client.util.ClientConfigFileManager;
-import com.victorgponce.permadeath_mod.util.ConfigFileManager;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.screen.world.SelectWorldScreen;
+import net.minecraft.client.network.CookieStorage;
+import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.TextWidget;
+import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.resource.language.I18n;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -15,8 +26,13 @@ import java.net.Socket;
 
 import static com.victorgponce.permadeath_mod.client.Permadeath_modClient.LOGGER;
 
+@Environment(EnvType.CLIENT)
 public class CustomMainMenu extends Screen {
     private TextWidget pingTextWidget;
+    private ButtonWidget playButton;
+    private ButtonWidget survivalButton;
+    private ButtonWidget optionsButton;
+    private ButtonWidget quitButton;
 
     ClientConfig clientConfig;
     private static String NODE;
@@ -27,20 +43,20 @@ public class CustomMainMenu extends Screen {
     private static final Identifier menuTitleId = Identifier.of("permadeath-mod", "textures/gui/title.png");
     private static final Identifier backgroundTextureId = Identifier.of("permadeath-mod", "textures/gui/background.png");
 
-    private String STATUS = "El servidor se encuentra OFFLINE";
+    private Text STATUS = Text.translatable("gui.permadeath_mod.status.offline");
     private static final long PING_CHECK_INTERVAL_MS = 5000; // Intervalo de 5 segundos
     private long lastPingCheckTime = 0; // Momento del último chequeo
     private Thread pingThread; // Hilo para la verificación del servidor
     private boolean stopPingThread = false; // Variable para detener el hilo cuando sea necesario
 
     public CustomMainMenu() {
-        super(Text.of("Main Menu"));
-        startPingThread();
+        super(Text.translatable("gui.permadeath_mod.title"));
 
         clientConfig = ClientConfigFileManager.readConfig();
         NODE = clientConfig.getServerAddress();
         PORT = clientConfig.getServerPort();
         SERVER_CHECK_ENABLED = clientConfig.isEnabledServerCheck();
+        startPingThread();
     }
 
     // Método para iniciar el hilo de verificación del servidor
@@ -48,9 +64,9 @@ public class CustomMainMenu extends Screen {
         pingThread = new Thread(() -> {
             while (!stopPingThread) {
                 if (Pinger(NODE, PORT)) {
-                    STATUS = "El servidor se encuentra ONLINE";
+                    STATUS = Text.translatable("gui.permadeath_mod.status.online");
                 } else {
-                    STATUS = "El servidor se encuentra OFFLINE";
+                    STATUS = Text.translatable("gui.permadeath_mod.status.offline");
                 }
                 // Esperar el intervalo de tiempo definido antes de la siguiente verificación
                 try {
@@ -77,10 +93,53 @@ public class CustomMainMenu extends Screen {
     protected void init() {
         super.init();
 
-        // Texto PingText alineado a la izquierda
-        pingTextWidget = new TextWidget(10, this.height - 20, this.textRenderer.getWidth(STATUS), 10, Text.of(STATUS), this.textRenderer);
+        String SERVER_ADDRESS = NODE + ":" + PORT;
 
+        // Obtener la mitad de la pantalla para centrar en el eje X
+        int centerX = this.width / 2;
+        int centerY = this.height / 2;
+
+        if (SERVER_CHECK_ENABLED) {
+            // Crear los botones con las nuevas posiciones
+            playButton = new ButtonWidget.Builder(Text.translatable("gui.permadeath_mod.play"), (buttonWidget) -> {
+                ServerInfo info = new ServerInfo(I18n.translate("selectServer.defaultName"), SERVER_ADDRESS, ServerInfo.ServerType.OTHER);
+                info.setResourcePackPolicy(ServerInfo.ResourcePackPolicy.PROMPT);
+                ConnectScreen.connect(this, this.client, ServerAddress.parse(SERVER_ADDRESS), info, true, (CookieStorage)null);
+                buttonWidget.playDownSound(this.client.getSoundManager());
+            }).dimensions(centerX - 100, centerY - 12, 200, 20).build(); // Ajustado hacia arriba
+        } else {
+            playButton = new ButtonWidget.Builder(Text.translatable("menu.multiplayer"), (buttonWidget) -> {
+                this.client.setScreen(new MultiplayerScreen(this));
+                buttonWidget.playDownSound(this.client.getSoundManager());
+            }).dimensions(centerX - 100, centerY - 12, 200, 20).build(); // Ajustado hacia arriba
+        }
+
+        // Crear el botón de Survival
+        survivalButton = new ButtonWidget.Builder(Text.translatable("menu.singleplayer"), (buttonWidget) -> {
+            this.client.setScreen(new SelectWorldScreen(this));
+            buttonWidget.playDownSound(this.client.getSoundManager());
+        }).dimensions(centerX - 100, centerY + 12, 200, 20).build(); // Ajustado hacia abajo
+
+        // Crear el botón de Opciones
+        optionsButton = new ButtonWidget.Builder(Text.translatable("menu.options"), (buttonWidget) -> {
+            MinecraftClient.getInstance().setScreen(new CustomOptionsScreen(this, this.client.options));
+            buttonWidget.playDownSound(this.client.getSoundManager());
+        }).dimensions(centerX - 100, centerY + 36, 200, 20).build(); // Ajustado hacia abajo
+
+        // Crear el botón de Salir
+        quitButton = new ButtonWidget.Builder(Text.translatable("menu.quit"), (buttonWidget) -> {
+            this.client.scheduleStop();
+            buttonWidget.playDownSound(this.client.getSoundManager());
+        }).dimensions(centerX - 100, centerY + 60, 200, 20).build(); // Ajustado hacia abajo
+
+        // Texto PingText alineado a la izquierda
+        pingTextWidget = new TextWidget(10, this.height - 20, this.textRenderer.getWidth(STATUS), 10, STATUS, this.textRenderer);
+
+        this.addDrawableChild(playButton);
         this.addDrawableChild(pingTextWidget);
+        this.addDrawableChild(survivalButton);
+        this.addDrawableChild(optionsButton);
+        this.addDrawableChild(quitButton);
     }
 
     @Override
@@ -102,7 +161,7 @@ public class CustomMainMenu extends Screen {
         long currentTimePing = System.currentTimeMillis();
 
         // Actualizar el contenido del TextWidget con el nuevo estado
-        pingTextWidget.setMessage(Text.of(STATUS));
+        pingTextWidget.setMessage(STATUS);
 
         // Renderizar la textura de fondo
         assert this.client != null;
