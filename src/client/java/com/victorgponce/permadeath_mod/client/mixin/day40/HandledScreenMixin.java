db9bf4b5ec6f4bb15db7fe033f8e10a554bda15c
ponchisao326_ <ponchisao326@hotmail.com>
2025-07-02 01:09:07 +0200

    Add HandledScreenMixin to disable off-hand key while interacting with screens

---
+++ src/client/java/com/victorgponce/permadeath_mod/client/mixin/day40/HandledScreenMixin.java
@@ -0,0 +1,30 @@
+package com.victorgponce.permadeath_mod.client.mixin.day40;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HandledScreen.class)
+public abstract class HandledScreenMixin {
+
+    /**
+     * This mixin disables the off-hand key (default 'F') in the HandledScreen class.
+     * It prevents the player from swapping items in their off-hand while interacting with a screen.
+     */
+    @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
+    private void disableOffHandKey(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        // Only runs on the client side
+        ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
+        // Check if the player is not null and if the swap hands key is pressed
+        if (player != null && MinecraftClient.getInstance().options.swapHandsKey.matchesKey(keyCode, scanCode)) {
+            cir.setReturnValue(true); // Prevent the default behavior of swapping hands
+            cir.cancel();
+        }
+    }
+
+}
